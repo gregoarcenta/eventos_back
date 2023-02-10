d4d2bac9effa374cf2c7ff6226d0d532bd6940f7
Gregory Arcentales <aarcentales@grupozurita.com.ec>
2023-02-10 09:40:12 -0500

    Se quitan comentarios en middleware de validacion de dominios

--- app/middlewares/authorizationMiddleware.js
+++ app/middlewares/authorizationMiddleware.js
@@ -1,12 +1,9 @@
 exports.validateDomains = (req, res, next) => {
   let domains = process.env.ENABLED_DOMAINS.split(",");
   try {
-    console.log("referer -> ", req.headers.referer);
-    console.log("referrer -> ", req.headers.referrer);
-
     const url = req.headers.referer || req.headers.referrer
     const authorized = domains.some((domain) => url === domain)
-    
+
     if (!req.headers.referer || !authorized) {
       res.status(403);
       throw new Error("Not authorized");
