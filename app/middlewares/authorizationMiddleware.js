84f96793e7e9225e1627266ecbb3364d5ca70008
Gregory Arcentales <aarcentales@grupozurita.com.ec>
2023-02-10 09:30:35 -0500

    cambios en middleware para validacion de dominios

--- app/middlewares/authorizationMiddleware.js
+++ app/middlewares/authorizationMiddleware.js
@@ -1,18 +1,13 @@
 exports.validateDomains = (req, res, next) => {
-  let whiteListDomain = process.env.ENABLED_DOMAINS.split(",");
+  let domains = process.env.ENABLED_DOMAINS.split(",");
   try {
     console.log("referer -> ", req.headers.referer);
     console.log("referrer -> ", req.headers.referrer);
-    if (!req.headers.referer) {
-      res.status(403);
-      throw new Error("Not authorized");
-    }
 
-    if (
-      !whiteListDomain.some(
-        (domain) => req.headers.referer.indexOf(domain) !== -1
-      )
-    ) {
+    const url = req.headers.referer || req.headers.referrer
+    const authorized = domains.some((domain) => url === domain)
+    
+    if (!req.headers.referer || !authorized) {
       res.status(403);
       throw new Error("Not authorized");
     }
