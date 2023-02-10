ce59cda7663e48b40820f91efb782f761831e0f7
Gregory Arcentales <aarcentales@grupozurita.com.ec>
2023-02-10 09:24:55 -0500

    .

--- app/middlewares/authorizationMiddleware.js
+++ app/middlewares/authorizationMiddleware.js
@@ -1,7 +1,8 @@
 exports.validateDomains = (req, res, next) => {
   let whiteListDomain = process.env.ENABLED_DOMAINS.split(",");
   try {
-    console.log("referer -> ", req.headers.referrer || req.headers.referer);
+    console.log("referer -> ", req.headers.referer);
+    console.log("referrer -> ", req.headers.referrer);
     if (!req.headers.referer) {
       res.status(403);
       throw new Error("Not authorized");
