b198be96d500f89195c941071698f7b4b196e1a9
Gregory Arcentales <aarcentales@grupozurita.com.ec>
2023-02-13 16:17:44 -0500

    prueba jenkins

--- app/middlewares/authorizationMiddleware.js
+++ app/middlewares/authorizationMiddleware.js
@@ -2,6 +2,7 @@ exports.validateDomains = (req, res, next) => {
   let domains = process.env.ENABLED_DOMAINS.split(",");
   try {
     const url = req.headers.referer || req.headers.referrer
+    console.log("url petición: ", url);
     const authorized = domains.some((domain) => url === domain)
 
     if (!req.headers.referer || !authorized) {
