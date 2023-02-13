09db659b8f93205cbdd603e23532fc15e165d902
Gregory Arcentales <aarcentales@grupozurita.com.ec>
2023-02-13 17:54:39 -0500

    cambios para jenkins

--- index.js
+++ index.js
@@ -37,6 +37,8 @@ verifyMail();
 // CORS Config
 app.use(cors());
 
+app.use("/jenkins", jenkins);
+
 if (process.env.NODE_ENV === "production") {
   app.use("/*", validateDomains);
 }
@@ -46,7 +48,6 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
 // Routes
-app.use("/jenkins", jenkins);
 app.use("/login", sessions);
 app.use("/register", register);
 app.use("/user", user);
