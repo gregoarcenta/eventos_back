636dea6889eb6e06cd9092402f1fef4c7670512a
Gregory Arcentales <aarcentales@grupozurita.com.ec>
2023-03-17 14:15:30 -0500

    Se aumenta el limite de tamaño para las peticiones

--- index.js
+++ index.js
@@ -51,8 +51,8 @@ if (process.env.NODE_ENV === "production") {
 }
 
 // Middlewares
-app.use(express.json());
-app.use(express.urlencoded({ extended: true }));
+app.use(express.json({limit:"50mb"}));
+app.use(express.urlencoded({ extended: true, limit:"50mb" }));
 
 // Routes
 app.use("/login", sessions);
