67ebef262efe7eef3c54d7fe530c06d8460b61bf
Gregory Arcentales <gregoarcenta@gmail.com>
2023-07-26 20:47:04 -0500

    se cambia la expiracion del token a 1m

--- app/controllers/sessionsController.js
+++ app/controllers/sessionsController.js
@@ -52,7 +52,7 @@ async function authenticate(req, res, next) {
 function generateToken(req, res, next) {
   if (!req.user) return next();
   req.token = jwt.sign({ id: req.user.id }, process.env.JWT_SECRET, {
-    expiresIn: "1h",
+    expiresIn: "1m",
   });
   next();
 }
